981124a25a00ae58128fcf9b1fb3f55e65626444
Moses613 <44594121+Moses613@users.noreply.github.com>
2018-12-24 00:10:15 -0500

    Update script.js
    add input validation

--- script.js
+++ script.js
@@ -7,5 +7,11 @@ function doTheMath() {
     measuredResult = parseFloat(arrayMaybe.elements[0].value);
     theoreticalResult = parseFloat(arrayMaybe.elements[1].value);
     percentDiff = Math.abs(((measuredResult - theoreticalResult)/theoreticalResult)) * 100;
-    alert("The percent difference is " + percentDiff + "%");
+    if (isNaN(percentDiff)) {
+        alert("Congratulations! You have broke my program! \nPlease try again with valid input.");
+    }
+    else {
+        alert("The percent difference is " + percentDiff + "%"); 
+    }
+    
 }
